b530aa58938cd703c5aa689f9553045412a7f68b
Jhow PC Agencia <jhonatanro.01@gmail.com>
2023-11-27 13:04:54 -0300

    Adição do arquivo JavaScript

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+const spanOne = document.getElementById('spanOne');
+const radioOne = document.getElementById('radioOne');
+
+spanOne.addEventListener('click', function () {
+    radioOne.checked = true;
+})
+
+const spanTwo = document.getElementById('spanTwo');
+const radioTwo = document.getElementById('radioTwo');
+
+spanTwo.addEventListener('click', function () {
+    radioTwo.checked = true;
+})
