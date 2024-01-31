79a62fd6f226a970d7cc24d15c03d0465092456f
Jhow PC Agencia <jhonatanro.01@gmail.com>
2024-01-31 16:12:15 -0300

    Melhorias no Javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,33 @@
+var formCpf = document.getElementById("formCpf");
+var formNumber = document.getElementById("formNumber");
+
 const spanOne = document.getElementById('spanOne');
 const radioOne = document.getElementById('radioOne');
-
-spanOne.addEventListener('click', function () {
-    radioOne.checked = true;
-})
 
 const spanTwo = document.getElementById('spanTwo');
 const radioTwo = document.getElementById('radioTwo');
 
+spanOne.addEventListener('click', function () {
+    radioOne.checked = true;
+    formNumber.classList.add("hidden");
+    formNumber.value = "";
+    formCpf.classList.remove("hidden");
+})
+
 spanTwo.addEventListener('click', function () {
     radioTwo.checked = true;
+    formCpf.classList.add("hidden");
+    formCpf.value = "";
+    formNumber.classList.remove("hidden");
 })
 
 
 function verificarCampo() {
 
-    const valorForm = document.getElementById('formNumber').value;
+    const valorFormOne = document.getElementById('formNumber').value;
+    const valorFormTwo = document.getElementById('formCpf').value;
 
-    if (valorForm === '') {
+    if (valorFormOne === '' && valorFormTwo === '') {
         alert(`Você deve preencher o campo primeiro`);
     } else {
 
